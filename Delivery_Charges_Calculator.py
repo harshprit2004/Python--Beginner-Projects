900cd18c334b5969ef1cbd8b5b879ac539c3aa44
Harshprit Singh <harshpritsinghsaini@gmail.com>
2025-06-20 14:40:02 +0530

    Create Delivery_Charges_Calculator.py

---
+++ Delivery_Charges_Calculator.py
@@ -0,0 +1,6 @@
+bill=float(input("Enter the Till Amount"))
+distance=float(input("Enter the distance of delivery"))
+delivery_charges=distance*2
+total_charges=delivery_charges+bill
+print("Delivery charges are:-",delivery_charges)
+print("total Bill is:-",total_charges)
